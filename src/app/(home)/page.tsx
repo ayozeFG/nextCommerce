50f0fa5e0d067fc6043e633f056adfaedda04abb
ayozeFG <ayoze.ferraz@gmail.com>
2023-08-25 23:57:55 +0100

    Pantalla con el pre-load inicial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,59 @@
-import { Typography } from '@mui/material'
+'use client'
+
+import { Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Image from 'next/image'
 
 export default function Home() {
+    const [showComponentA, setShowComponentA] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowComponentA(!showComponentA);
+        }, 2500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className='center'>
-            <Typography variant='h1'>
-                PALITOS
-            </Typography>
-        </div>
+        showComponentA ? (
+            <div className="container">
+                <div className='clip-pathD'>
+                    <Image
+                        className='background-image'
+                        src="/fondo.jpg"
+                        priority
+                        layout="fill"
+                        alt="Luxed stones"
+                        />
+                </div>
+                <div className='clip-path'>
+                    <Image
+                        className='background-image'
+                        src="/fondo.jpg"
+                        priority
+                        layout="fill"
+                        alt="Luxed stones"
+                        />
+                </div>
+                <div className='clip-pathI'>
+                    <Image
+                        className='background-image'
+                        src="/fondo.jpg"
+                        priority
+                        layout="fill"
+                        alt="Luxed stones"
+                        />
+                </div>
+            </div>
+        ) : (
+            <div className="container2 autoFadeIn">
+                <div className='center'>
+                    <Typography variant='h1'>
+                        PALITOS
+                    </Typography>
+                </div>
+            </div>
+        )
     )
 }
